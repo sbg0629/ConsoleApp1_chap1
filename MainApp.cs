222c6dbdeb7206d9b3559462916769b813a7fce1
손봉균 <124877603+sbg0629@users.noreply.github.com>
2023-07-06 13:29:05 +0900

    c#기본 틀과 변수

---
+++ MainApp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 100000;
+            int b = 1000;
+            int c = 2000;
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c); 
+        }
+    }
+}
